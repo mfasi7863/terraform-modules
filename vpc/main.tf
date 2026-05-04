0e390f7a2213f4325613ed3ea3542ef15198f039
EC2 Default User <ec2-user@ip-172-31-47-200.ap-south-1.compute.internal>
2026-05-04 21:54:13 +0000

    Fixed AZ's issue

--- vpc/main.tf
+++ vpc/main.tf
@@ -19,3 +19,29 @@ resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnets[count.index]
 }
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
+
+  availability_zone = local.azs[count.index]
+
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count = 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(["10.0.3.0/24", "10.0.4.0/24"], count.index)
+
+  availability_zone = local.azs[count.index]
+}
