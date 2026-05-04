08f17b8bd1c823e1e2c1d5c0cda5731c6280a57e
EC2 Default User <ec2-user@ip-172-31-47-200.ap-south-1.compute.internal>
2026-05-04 20:49:49 +0000

    Fix EKS IAM role

--- eks/main.tf
+++ eks/main.tf
@@ -1,6 +1,21 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eksClusterRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
 resource "aws_eks_cluster" "cluster" {
   name     = var.cluster_name
-  role_arn = "arn:aws:iam::672296383659:role/EKSRole"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = var.subnet_ids
