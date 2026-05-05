6329f451d0a1e784a36365d017e202930a67c82f
EC2 Default User <ec2-user@ip-172-31-47-200.ap-south-1.compute.internal>
2026-05-05 12:33:40 +0000

    Updated subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -6,42 +6,31 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "public" {
-  count = length(var.public_subnets)
-
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnets[count.index]
-}
-
-resource "aws_subnet" "private" {
-  count = length(var.private_subnets)
-
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnets[count.index]
-}
-
+# Get AZs
 data "aws_availability_zones" "available" {}
 
 locals {
   azs = slice(data.aws_availability_zones.available.names, 0, 2)
 }
 
+# Public Subnets (Multi-AZ)
 resource "aws_subnet" "public" {
   count = 2
 
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
+  cidr_block = element(var.public_subnets, count.index)
 
   availability_zone = local.azs[count.index]
 
   map_public_ip_on_launch = true
 }
 
+# Private Subnets (Multi-AZ)
 resource "aws_subnet" "private" {
   count = 2
 
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(["10.0.3.0/24", "10.0.4.0/24"], count.index)
+  cidr_block = element(var.private_subnets, count.index)
 
   availability_zone = local.azs[count.index]
 }
